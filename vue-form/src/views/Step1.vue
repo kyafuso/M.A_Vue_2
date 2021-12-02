74e8108105a27f240df58b5dc6070e46952906fc
kyafuso <kazuya.yafuso.0825@gmail.com>
2021-12-02 12:51:36 +0900

    年月日の生成処理をdefinition.jsに移行

--- vue-form/src/views/Step1.vue
+++ vue-form/src/views/Step1.vue
@@ -57,13 +57,15 @@
 </template>
 
 <script>
+import {years, months, days} from "../definition.js"
+
 export default {
   name: 'Step1',
   data: function(){
     return {
-      years : [],
-      months : [],
-      days : []
+      years : years,
+      months : months,
+      days : days
     }
   },
   computed : {
@@ -98,35 +100,6 @@ export default {
       set(value) {
         this.$store.commit('setDay', value);
       }
-    }
-  },
-  created() {
-    const year = 1960
-    for (let i = 0; i < 62; i++) {
-      
-      let seireki = year + i;
-      let wareki = '';
-
-      if (seireki > 2018) { 
-        wareki = '令和' + (seireki - 2018);
-      } else if (seireki > 1988) {
-        wareki = '平成' + (seireki - 1988);
-      } else if (seireki > 1925) {
-        wareki = '昭和' + (seireki - 1925);
-      } 
-
-      this.years.push( {
-          seireki : seireki,
-          wareki : wareki
-        })
-    }
-    const month = 1
-    for (let i = 0; i < 12; i++) {
-      this.months.push(month + i)
-    }
-    const day = 1
-    for (let i = 0; i < 31; i++) {
-      this.days.push(day + i)
     }
   }
 }
